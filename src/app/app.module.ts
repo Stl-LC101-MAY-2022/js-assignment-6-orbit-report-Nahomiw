8c82013a4cf89ae0ccaf82a91e864491db3cff72
Nahomiw <nahomib@gmail.com>
2022-08-17 21:38:59 -0500

    assignment6completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 
+
+
 import { AppComponent } from './app.component';
+
 import { OrbitListComponent } from './orbit-list/orbit-list.component';
+
 import { OrbitCountsComponent } from './orbit-counts/orbit-counts.component';
 
+
+
 @NgModule({
+
   declarations: [
+
     AppComponent,
+
     OrbitListComponent,
+
     OrbitCountsComponent
+
   ],
+
   imports: [
+
     BrowserModule
+
   ],
+
   providers: [],
+
   bootstrap: [AppComponent]
+
 })
+
 export class AppModule { }
+
--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,23 +1,53 @@
 export class Satellite {
 
+
+
 	name: string;
+
 	type: string;
+
 	launchDate: string;
+
 	orbitType: string;
+
 	operational: boolean;
 
+
+
 	constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+
 		this.name = name;
+
 		this.type = type;
+
 		this.launchDate = launchDate;
+
 		this.orbitType = orbitType;
+
 		this.operational = operational;
-   }
-	
+
+	}
+
+
+
 	isSpaceDebris(): boolean {
-		return true;
-   }
+
+		if (this.type === 'Space Debris') {
+
+			return true;
+
+		} else {
+
+			return false;
+
+		}
+
+	}
+
+
 
 }
+
+
 
 // TODO 3a: fix isSpaceDebris check
